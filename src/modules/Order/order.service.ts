a8975c824906b344469da3af2f7146d8ab070337
Hussien <husseinfayed86@gmail.com>
2023-02-02 16:19:54 +0200

    Order Fixes

--- src/modules/Order/order.service.ts
+++ src/modules/Order/order.service.ts
@@ -10,63 +10,73 @@ import { Recipe } from '../../models/recipe.entity';
 import { LOADIPHLPAPI } from 'dns';
 
 @Injectable()
-export class OrderService extends AbstractService<Order>{
+export class OrderService extends AbstractService<Order> {
     constructor(
         @InjectRepository(Order) private readonly repo: Repository<Order>,
         @InjectRepository(Cart) private readonly cartRepo: Repository<Cart>,
-        @InjectRepository(Product) private readonly productRepo: Repository<Product>,
+        @InjectRepository(Product)
+        private readonly productRepo: Repository<Product>,
         @InjectRepository(Recipe) private readonly recipeRepo: Repository<Recipe>,
-        private readonly userservice: UserService
+        private readonly userservice: UserService,
     ) {
-        super(repo)
+        super(repo);
     }
 
     async makeOrder(req) {
-
-        const user_name = req.user.name
+        const user_name = req.user.name;
         console.log('User Name :', user_name);
-        const order_number = Math.floor((Math.random() * 1000) + 1)
+        const order_number = Math.floor(Math.random() * 1000 + 1);
         console.log('Order Number =', order_number);
 
         const newRecipe = await this.recipeRepo.save({
             user_name: user_name,
             order_number: order_number,
             status: 'PENDING',
-        })
+        });
 
-        const user_product = await this.cartRepo.createQueryBuilder('p')
+        const user_product = await this.cartRepo
+            .createQueryBuilder('p')
             .where('p.user_name = :user_name', { user_name })
-            .select(['p.productId']).getMany()
+            .select(['p.productId'])
+            .getMany();
 
         user_product.forEach(async (x) => {
-            const productId = x.productId
+            const productId = x.productId;
             console.log('Product id', productId);
 
-            const stock = await this.productRepo.createQueryBuilder('p')
+            const stock = await this.productRepo
+                .createQueryBuilder('p')
                 .where('p.id = :id', { id: productId })
-                .select(['p.stock']).getMany()
+                .select(['p.stock'])
+                .getMany();
             console.log('Stock =', stock[0].stock);
 
-            const qty = await this.cartRepo.createQueryBuilder('p')
+            const qty = await this.cartRepo
+                .createQueryBuilder('p')
                 .where('p.user_name = :user_name', { user_name })
                 .andWhere('p.productId = :productId', { productId })
-                .select(['p.qty']).getMany()
+                .select(['p.qty'])
+                .getMany();
             console.log('Quantity =', qty[0].qty);
 
-            const updated_stock = Number(stock[0].stock) - Number(qty[0].qty)
-            if (updated_stock < 0)
-                return "Out Of Stock"
+            const updated_stock = Number(stock[0].stock) - Number(qty[0].qty);
+            if (updated_stock < 0) return 'Out Of Stock';
             console.log('Updated Stock =', updated_stock);
 
-            // await this.productRepo.update({ id: productId }, { stock: updated_stock })
+            await this.productRepo.update(
+                { id: productId },
+                { stock: updated_stock },
+            );
 
-            const price = await this.cartRepo.createQueryBuilder('p')
+            const price = await this.cartRepo
+                .createQueryBuilder('p')
                 .where('p.user_name = :user_name', { user_name })
                 .andWhere('p.productId = :productId', { productId })
-                .select(['p.price']).getMany()
+                .select(['p.price'])
+                .getMany();
             console.log('Price =', price[0].price);
 
-            const total_price = price[0].price * qty[0].qty
+            const total_price = price[0].price * qty[0].qty;
             console.log('Total Price =', total_price);
 
             const newOrder = await this.repo.save({
@@ -77,22 +87,21 @@ export class OrderService extends AbstractService<Order>{
                 total_price: total_price,
                 order_number: order_number,
                 //sellerName: sellerName[0].user[0],
-            })
+            });
 
-            //     // // await this.cartRepo.delete({ user_name: user_name })
+            await this.cartRepo.delete({ user_name: user_name });
 
-            var recipe_number = await this.recipeRepo.createQueryBuilder('p')
+            var recipe_number = await this.recipeRepo
+                .createQueryBuilder('p')
                 .where('p.order_number = :order_number', { order_number })
-                .select(['p.id']).getMany()
+                .select(['p.id'])
+                .getMany();
             console.log('Recipe Number =', recipe_number[0].id);
 
-            await this.repo.update({ order_number: order_number }, { recipe_number: recipe_number[0].id })
-        })
-
+            await this.repo.update(
+                { order_number: order_number },
+                { recipe_number: recipe_number[0].id },
+            );
+        });
     }
 }
-
-
-
-
-
